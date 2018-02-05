4ec61e2e9f8b2933a332dc89ad02cce28dacd885
Gaurav Goel <gaurav.goel167@gmail.com>
2018-02-05 20:18:00 +0530

    Added STATIC_ROOT

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w1i$2a@5h49b9ai*t)&os85wt8d3xl_5&^=@_ng7=vh*+-9uxd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','pure-dusk-51790.herokuapp.com']
 
 
 # Application definition
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
